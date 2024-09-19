3f99d2c50a208f416a2521a6cb542d5020aff8c7
rice-mountain <ricemountain.dev@gmail.com>
2024-09-19 22:42:29 +0900

    modified

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const Joi = require("joi");
 const session = require("express-session");
+const flash = require("connect-flash");
 
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
-const app = express();
 
+//DB connection
 mongoose
   .connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
@@ -27,30 +27,45 @@ mongoose
     console.log(err);
   });
 
+const app = express();
+
 app.engine("ejs", ejsMate);
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
 //static file setting
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/reviews", reviewRoutes);
-
+// session
 const sessionConfig = {
   secret: "mysecret",
   resave: false,
   saveUninitialized: true,
   cookie: {
+    httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
 app.use(session(sessionConfig));
 
+// // flash
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+//route
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError("page is not found", 404));
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -32,6 +32,10 @@ router.get(
   "/:id",
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate("reviews");
+    if (!campground) {
+      req.flash("error", "キャンプ場は見つかりませんでした");
+      res.redirect("/campgrounds");
+    }
     res.render("campgrounds/show", { campground });
   })
 );
@@ -42,6 +46,7 @@ router.post(
   catchAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash("success", "新しいキャンプ場を登録しました");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -50,6 +55,10 @@ router.get(
   "/:id/edit",
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash("error", "キャンプ場は見つかりませんでした");
+      res.redirect("/campgrounds");
+    }
     res.render("campgrounds/edit", { campground });
   })
 );
@@ -60,6 +69,7 @@ router.put(
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash("success", "キャンプ場を更新しました");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -69,6 +79,7 @@ router.delete(
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash("success", "キャンプ場を削除しました");
     res.redirect("/campgrounds");
   })
 );
