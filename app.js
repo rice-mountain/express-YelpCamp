f6b6d4b3f1dfcdc7c0a2b21843d8b8718cc7b0b7
rice-mountain <ricemountain.dev@gmail.com>
2024-09-28 10:44:38 +0900

    sec

--- app.js
+++ app.js
@@ -14,14 +14,24 @@ const User = require("./models/user");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 
+// sqlインジェクション対策のpackage
+const mongoSanitize = require('express-mongo-sanitize');
+
 //route
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const userRoutes = require("./routes/users");
+// httpヘッダーにセキュリティ対策
+const { default: helmet } = require("helmet");
+
+// session Store用
+const mongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 
 //DB connection
+const dbUrl = "mongodb://localhost:27017/yelp-camp"
 mongoose
-  .connect("mongodb://localhost:27017/yelp-camp", {
+  .connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -50,12 +60,25 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // session
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypt: {
+    secret: 'mysecret'
+  },
+  touchAfter: 24 * 3600
+})
+
+store.on('error', e => console.log('セッションストアエラー'))
+
 const sessionConfig = {
+  store: store,
+  name: 'session', //sessionのdefaultの名前を変える（未指定だとpassportの場合はconnect_sid）
   secret: "mysecret",
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    //secure: true, //sessionのやりとりをhttpsでのみ許可（localhostでログインできなくなるためコメントアウト）
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
@@ -71,6 +94,8 @@ passport.deserializeUser(User.deserializeUser());
 // // flash
 app.use(flash());
 
+// app.use(helmet());
+
 // 毎回のリクエストで処理
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
@@ -78,6 +103,9 @@ app.use((req, res, next) => {
   res.locals.error = req.flash("error");
   next();
 });
+
+//sqlインジェクション対策 クエリパラメータをサニタイズ
+app.use(mongoSanitize());
 
 app.get("/", (req, res) => {
   res.render("home");
